32021008e4fad352fb6a3b487e94ab7de969a1e7
candide guevara <candide-guevara@users.noreply.github.com>
2018-03-18 08:51:43 +0100

    radix sort work in progress

--- sorting_in_1mb_ram/radix_sorting.cpp
+++ sorting_in_1mb_ram/radix_sorting.cpp
@@ -7,110 +7,157 @@
 #include <sstream>
 
 RadixTree::RadixTree() 
-  : chunk(lvl1_len, slot_empty) { } 
+    : chunk(lvl0_len, slot_empty) { } 
+
+uint32_t RadixTree::to_chunk_payload(uint32_t number) const {
+    auto payload = (number >> lvl0_shf) + slot_min_val;
+    MY_ASSERT(payload < lvl0_flag);
+    return (payload | lvl0_flag);
+}
+
+uint32_t RadixTree::from_chunk_payload(uint32_t slot) const {
+    auto number = (chunk[slot] & (~lvl0_flag)) - slot_min_val;
+    number = (number << lvl0_shf) + slot;
+    MY_ASSERT(number <= max_number);
+    return number;
+}
+
+uint32_t RadixTree::add_to_last_resort(uint32_t number) {
+    auto end = last_resort.end();
+    auto it = algo(lower_bound, last_resort, number);
+    if(it != end)
+        std::move_backward(it, end-1, end);
+
+    *it = number;
+    return slot_min_val;
+}
 
 uint32_t RadixTree::add_number(uint32_t number) {
-  auto slot = (number >> lvl1_shf);
-  auto payload = (number & lvl1_mask);
-  MY_ASSERT(slot < lvl1_len);
+    MY_ASSERT(number <= max_number);
+    auto slot = (number & lvl0_mask);
 
-  if(chunk[slot] == slot_empty)
-    chunk[slot] = (payload | lvl0_flag);
-  else if(chunk[slot] & lvl0_flag) {
-    auto previous = (chunk[slot] & lvl0_mask);
-    auto id = next_lvl.add_number(previous);
-    chunk[slot] = id;
-    id = next_lvl.add_number(id, number);
-    MY_ASSERT(id != slot_empty && id == chunk[slot] && !(chunk[slot] & lvl0_flag));
-  }
-  else {
+    if(chunk[slot] == slot_empty) {
+        chunk[slot] = to_chunk_payload(number);
+        return slot_min_val;
+    }
+    else if(chunk[slot] & lvl0_flag) {
+        auto previous = from_chunk_payload(slot);
+        auto id = next_lvl.add_number(previous);
+        chunk[slot] = id;
+        MY_ASSERT(id != slot_empty && !(chunk[slot] & lvl0_flag));
+    }
+
     auto id = next_lvl.add_number(chunk[slot], number);
     if(id == slot_empty) {
-      last_resort.push_back(number);
-      return slot_empty;
+        add_to_last_resort(number);
+        return slot_empty;
     }
-    MY_ASSERT(id == chunk[slot] && !(chunk[slot] & lvl0_flag));
-  }
-  return slot_min_val;
+    return slot_min_val;
 }
 
-ItContainer<RadixLvl1It> RadixTree::range() {
-  RadixIt begin{}, end{};
-  algo_srt(last_resort);
+ItContainer<RadixIt> RadixTree::range() const {
+    RadixIt begin{this, 
+        last_resort.begin(),
+        chunk.begin(),
+    };
+    begin.advance_chunk_skip_empty(false);
 
-  begin.last_resort_it = last_resort.begin();
-  begin.chunk_begin = begin.chunk_it = chunk.begin();
-  begin.last_resort_end = end.last_resort_end = last_resort.end();
-  begin.chunk_end = end.chunk_end = chunk.end();
+    RadixIt end{this};
+    end.move_to_end();
 
-  while(*(begin.chunk_it) == slot_empty && begin.chunk_it != chunk_end)
-    ++begin.chunk_it;
-  if(begin.chunk_it != chunk_end) {
-    auto next_range = next_lvl.range(*(begin.chunk_it));
-    begin.next_it = next_range.begin();
-    begin.next_end = end.next_end = next_range.end();
-  }
-  else {
-    auto next_range = next_lvl.range(slot_empty);
-    begin.next_end = end.next_end = next_range.end();
-    begin.move_to_end();
-  }
-
-  end.move_to_end();
-  ItContainer<RadixIt> begin_end{ begin, end };
-  return begin_end;
+    ItContainer<RadixIt> begin_end{ begin, end };
+    return begin_end;
 }
 
-uint32_t RadixIt::extract(uint32_t number) {
-  auto dist = std::distance(chunk_begin, chunk_it);
-  return (dist << lvl0_shf) + number;
+uint32_t RadixIt::extract_direct() const {
+    auto dist = std::distance(parent->chunk.begin(), chunk_it);
+    return parent->from_chunk_payload(dist);
 }
 
-reference RadixIt::operator*() {
-  if(next_it != next_end) {
-    auto next_val = extract(*next_it);
-    if(last_resort_it != last_resort_end) {
-      if(*last_resort_it < next_val) return *last_resort;
-      else return next_val;
+uint32_t RadixIt::extract_lower_lvl() const {
+    auto dist = std::distance(parent->chunk.begin(), chunk_it);
+    auto number = (*next_it << lvl0_shf) + dist;
+    MY_ASSERT(number <= max_number);
+    return number;
+}
+
+RadixIt::reference RadixIt::operator*() const {
+    if(chunk_it != parent->chunk.end() && (*chunk_it & lvl0_flag)) {
+        auto res = extract_direct();
+        MY_ASSERT(next_it == parent->next_lvl.empty_it());
+        MY_ASSERT(last_resort_it != parent->last_resort.end() || *last_resort_it >= res); 
+        return res;
     }
-    else return next_val;
-  }
-  MY_ASSERT(last_resort_it != last_resort_end); 
-  return *last_resort;
+
+    if(next_it != next_end) {
+        auto next_val = extract_lower_lvl();
+        if(last_resort_it == parent->last_resort.end() || *last_resort_it > next_val) 
+            return next_val;
+    }
+
+    MY_ASSERT(next_it != next_end || next_it == parent->next_lvl.empty_it());
+    MY_ASSERT(last_resort_it != parent->last_resort.end()); 
+    return *last_resort_it;
 }
 
-RadixIt&  RadixIt::operator++() {
-  auto cur = **this;
-  if(last_resort_it != last_resort_end && cur == *last_resort_it) 
+void RadixIt::advance_chunk_skip_empty(bool preincrement) {
+    if(preincrement) ++chunk_it;
+    while(chunk_it != parent->chunk.end() && *chunk_it == slot_empty)
+        ++chunk_it;
+
+    if(chunk_it != parent->chunk.end()) {
+        if(*chunk_it & lvl0_flag) {
+            next_it  = parent->next_lvl.empty_it();
+            next_end = parent->next_lvl.empty_it();
+        }
+        else {
+            auto next_range = parent->next_lvl.range(*chunk_it);
+            next_it = next_range.begin();
+            next_end = next_range.end();
+        }
+    }
+    else move_to_end();
+}
+
+RadixIt& RadixIt::operator ++ () {
+    if(chunk_it != parent->chunk.end() && (*chunk_it & lvl0_flag)) {
+        MY_ASSERT(next_it == parent->next_lvl.empty_it());
+        advance_chunk_skip_empty();    
+    }
+
+    if(next_it != next_end) {
+        MY_ASSERT(chunk_it != parent->chunk.end());
+        auto next_val = extract_lower_lvl();
+
+        if(last_resort_it == parent->last_resort.end() || *last_resort_it > next_val)
+            advance_chunk_skip_empty();    
+    }
+
+    MY_ASSERT(next_it != next_end || next_it == parent->next_lvl.empty_it());
+    MY_ASSERT(last_resort_it != parent->last_resort.end()); 
     ++last_resort_it;
-  else if(next_it != next_end && cur == *next_it) {
-    ++next_it;
-    if(next_it == next_end) {
-      ++chunk_it;
-      next_it = RadixLvl1It()
-    }
-  }
-  return *this;
+    return *this;
 }
 
-RadixIt   RadixIt::operator++(int) {
-  RadixIt it {*this};
-  ++it;
-  return it;
+RadixIt RadixIt::operator++(int) {
+    RadixIt it {*this};
+    ++it;
+    return it;
 }
 
 bool RadixIt::operator==(const RadixIt& rhs) const {
-  return last_resort_it == rhs.last_resort_it
-    && next_it == rhs.next_it
-    && chunk_it == rhs.chunk_it;
+    MY_ASSERT(parent == rhs.parent);
+    return last_resort_it == rhs.last_resort_it
+        && next_it == rhs.next_it
+        && chunk_it == rhs.chunk_it;
 }
 
 bool RadixIt::operator!=(const RadixIt& rhs) const { return !(*this == rhs); }
 
 void RadixIt::move_to_end() {
-  last_resort_it = last_resort_end;
-  next_it = next_end;
-  chunk_it = chunk_end;
+    last_resort_it = parent->last_resort.end();
+    chunk_it = parent->chunk.end();
+    next_it = next_end = parent->next_lvl.empty_it();
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -118,137 +165,273 @@ void RadixIt::move_to_end() {
 RadixLevel1::RadixLevel1() {}
 
 uint32_t RadixLevel1::add_number(uint32_t number) {
-  RadixLevel2 lvl2;
-  auto id = lvl2.add_number(number);
-  MY_ASSERT(id != slot_empty);
-  chunks.push_back(lvl2);
-  return chunks.size() - 1 + slot_min_val;
+    chunks.emplace_back();
+    MY_ASSERT(chunks.size() < lvl0_len);
+    auto id = chunks.back().add_number(number);
+    MY_ASSERT(id != slot_empty);
+    return chunks.size() - 1 + slot_min_val;
 }
 
 uint32_t RadixLevel1::add_number(uint32_t id, uint32_t number) {
-  id -= slot_min_val;
-  MY_ASSERT(id < chunks.size());
-  auto id = chunks[id].add_number(number);
-  return id;
-}
-
-ItContainer<RadixLvl1It> RadixLevel1::range(uint32_t id) {
-  RadixLvl2It next_it, next_end;
-  RadixLvl1It begin{}, end{};
-
-  begin.id = end.id = id;
-  begin.consumed = end.consumed = false;
-
-  if(!(id & lvl0_flag)) { 
     id -= slot_min_val;
+    MY_ASSERT(id < chunks.size());
+    id = chunks[id].add_number(number);
+    return id;
+}
+
+RadixLvl1It RadixLevel1::empty_it() const { 
+    return RadixLvl1It{this, lvl0_len}; 
+}
+
+ItContainer<RadixLvl1It> RadixLevel1::range(uint32_t id) const {
+    id -= slot_min_val;
+    MY_ASSERT(id < chunks.size());   
+
     auto next_range = chunks[id].range();
-    begin.next_it = next_range.begin();
-    begin.next_end = end.next_end = next_range.end();
-  }
+    MY_ASSERT(next_range.begin() != next_range.end());
+    RadixLvl1It begin{this, id, next_range.begin(), next_range.end()};
+    RadixLvl1It end{begin};
+    end.move_to_end();
 
-  end.move_to_end();
-  ItContainer<RadixLvl1It> begin_end{ begin, end };
-  return begin_end;
+    ItContainer<RadixLvl1It> begin_end{ begin, end };
+    return begin_end;
 }
 
-reference     RadixLvl1It::operator*() {
-  if(id & lvl0_flag) 
-    return (id & lvl0_mask);
-  return *next_it;
+RadixLvl1It::reference RadixLvl1It::operator*() const {
+    MY_ASSERT(next_it != next_end && id < lvl0_len);
+    return *next_it;
 }
 
-RadixLvl1It&  RadixLvl1It::operator++() {
-  if(id & lvl0_flag) {
-    MY_ASSERT(!consumed);
-    consumed = true;
-  }
-  else ++next_it;
-  return *this;
+RadixLvl1It& RadixLvl1It::operator++() {
+    MY_ASSERT(next_it != next_end && id < lvl0_len);
+    ++next_it;
+    return *this;
 }
 
-RadixLvl1It   RadixLvl1It::operator++(int) {
-  RadixLvl1It it {*this};
-  ++it;
-  return it;
+RadixLvl1It RadixLvl1It::operator++(int) {
+    RadixLvl1It it {*this};
+    ++it;
+    return it;
 }
 
-bool RadixLvl1It::operator==(const RadixIt& rhs) const {
-  return ((id & lvl0_flag) && consumed == rhs.consumed && id == rhs.id)
-    || (next_it == rhs.next_it);
+bool RadixLvl1It::operator==(const RadixLvl1It& rhs) const {
+    MY_ASSERT(parent == rhs.parent);
+    return (id == rhs.id && id == lvl0_len)
+        || (id == rhs.id && next_it == rhs.next_it);
 }
 
-bool RadixLvl1It::operator!=(const RadixIt& rhs) const { return !(*this == rhs); }
+bool RadixLvl1It::operator!=(const RadixLvl1It& rhs) const { return !(*this == rhs); }
 
 void RadixLvl1It::move_to_end() {
-  consumed = true;
-  next_it = next_end;
+    MY_ASSERT(id < lvl0_len);
+    next_it = next_end;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 RadixLevel2::RadixLevel2() 
-  : chunk {{slot_empty}} {}
+    : chunk {{slot_empty}}, extra{{slot_empty}}, extra_top{} {}
+
+uint32_t RadixLevel2::to_chunk_payload(uint32_t number) const {
+    auto payload = (number >> (lvl2_shf + lvl0_shf)) + slot_min_val;
+    MY_ASSERT(payload < lvl2_flag);
+    return payload;
+}
+
+uint32_t RadixLevel2::to_extra_payload(uint32_t number) const {
+    auto payload = (number >> lvl0_shf) + slot_min_val;
+    MY_ASSERT(payload < lvl0_flag);
+    return payload;
+}
 
 uint32_t RadixLevel2::add_number(uint32_t number) {
-  auto slot = (number & lvl0_mask) >> lvl2_shf;
-  auto payload = (number & lvl2_mask);
+    auto slot = (number >> lvl0_shf) & lvl2_mask;
 
-  if(chunk[slot] == slot_empty)
-    chunk[slot] = (payload | lvl2_flag);
-  else if(chunk[slot] & lvl2_flag) {
-    auto previous = (chunk[slot] & lvl2_mask);
-    auto id = next_lvl.add_number(previous);
-
-    if(id == slot_empty) return slot_empty;
-
-    chunk[slot] = id;
-    id = next_lvl.add_number(id, number);
-    MY_ASSERT(id == chunk[slot] && !(chunk[slot] & lvl2_flag));
-  }
-  else {
-    auto id = next_lvl.add_number(chunk[slot], number);
-    if(id == slot_empty) return slot_empty;
-    MY_ASSERT(id == chunk[slot] && !(chunk[slot] & lvl2_flag));
-  }
-  return slot_min_val;
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-RadixLevel3::RadixLevel3() {}
-
-uint32_t RadixLevel3::add_number(uint32_t number) {
-  if (chunks.size() >= lvl2_mask)
-    return slot_empty;
-  std::array<uint8_t, lvl3_len> chunk {{slot_empty}};
-  auto payload = (number & lvl2_mask);
-  chunk[0] = payload;
-  chunks.push_back(chunk);
-  return chunks.size() - 1 + slot_min_val;
-}
-
-uint32_t RadixLevel3::add_number(uint32_t id, uint32_t number) {
-  id -= slot_min_val;
-  MY_ASSERT(id < chunks.size());
-  auto payload = (number & lvl2_mask);
-
-  auto& chunk = chunks[id];
-  auto it = std::find(chunk.begin(), chunk.end(), slot_empty);
-  if (it != chunk.end()) {
-    *it = (payload | lvl2_flag);
+    if(chunk[slot] == slot_empty) {
+        auto payload = to_chunk_payload(number);
+        chunk[slot] = payload;
+        return slot_min_val;
+    }
+    auto id = add_to_extra(number);
     return id;
-  }
-  return slot_empty;
+}
+
+uint32_t RadixLevel2::add_to_extra(uint32_t number) {
+    if(extra_top == extra.size())
+        return slot_empty;
+
+    auto payload = to_extra_payload(number);
+    auto end = extra.begin() + extra_top;
+    auto it = std::lower_bound(extra.begin(), end, payload);
+    if(it != end)
+        std::move_backward(it, end-1, end);
+
+    *it = payload;
+    ++extra_top;
+    return slot_min_val;
+}
+
+ItContainer<RadixLvl2It> RadixLevel2::range() const {
+    RadixLvl2It begin{this, chunk.begin(), extra.begin()};
+    begin.advance_chunk_skip_empty(false);
+    RadixLvl2It end{this};
+    end.move_to_end();
+
+    MY_ASSERT(begin.chunk_it != chunk.end() 
+        || begin.extra_it == extra.begin() + extra_top);
+    ItContainer<RadixLvl2It> begin_end{ begin, end };
+    return begin_end;
+}
+
+void RadixLvl2It::advance_chunk_skip_empty(bool preincrement) {
+    if(preincrement) ++chunk_it;
+    while(chunk_it != parent->chunk.end() && *chunk_it == slot_empty)
+        ++chunk_it;
+}
+
+uint32_t RadixLvl2It::extract_from_chunk() const {
+    auto dist = std::distance(parent->chunk.begin(), chunk_it);
+    auto result = ((*chunk_it - slot_min_val) << lvl2_shf) + dist;
+    MY_ASSERT(result < lvl0_flag);
+    return result; 
+}
+
+uint32_t RadixLvl2It::extract_from_extra() const {
+    auto result = *extra_it - slot_min_val;
+    MY_ASSERT(result < lvl0_flag);
+    return result; 
+}
+
+RadixLvl2It::reference RadixLvl2It::operator*() const {
+    auto extra_end = parent->extra.begin() + parent->extra_top;
+    auto result = slot_empty;
+
+    if(chunk_it != parent->chunk.end()) {
+        auto result = extract_from_chunk();
+        if(extra_it != extra_end)
+            result = std::max(extract_from_extra(), result);
+    }
+    else if(extra_it != extra_end)
+        result = extract_from_extra();
+    else MY_ASSERT(false);
+    return result;
+}
+
+RadixLvl2It& RadixLvl2It::operator++() {
+    auto extra_end = parent->extra.begin() + parent->extra_top;
+
+    if(chunk_it != parent->chunk.end()) {
+        auto chunk_val = extract_from_chunk();
+        if(extra_it != extra_end && chunk_val > extract_from_extra())
+            ++extra_it;
+        else advance_chunk_skip_empty();
+    }
+    else if(extra_it != extra_end)
+        ++extra_it;
+    else MY_ASSERT(false);
+    return *this;
+}
+
+RadixLvl2It RadixLvl2It::operator++(int) {
+    RadixLvl2It it {*this};
+    ++it;
+    return it;
+}
+
+bool RadixLvl2It::operator==(const RadixLvl2It& rhs) const {
+    MY_ASSERT(parent == rhs.parent && parent->extra_top == rhs.parent->extra_top);
+    return chunk_it == rhs.chunk_it && extra_it == rhs.extra_it;
+}
+
+bool RadixLvl2It::operator!=(const RadixLvl2It& rhs) const { return !(*this == rhs); }
+
+void RadixLvl2It::move_to_end() {
+    auto extra_end = parent->extra.begin() + parent->extra_top;
+    chunk_it = parent->chunk.end();
+    extra_it = extra_end;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-void sort_numbers_by_radix() {}
+void RadixStats::calculate_on(RadixTree& level) {
+    free_lv0 = algo(count_if, level.chunk, [](uint32_t v) { return v == slot_empty; });
+    take_lv0 = algo(count_if, level.chunk, [](uint32_t v) { return v & lvl0_flag; });
+    chld_lv0 = lvl0_len - take_lv0 - free_lv0;
+    calculate_on(level.next_lvl);
+}
+
+void RadixStats::calculate_on(RadixLevel1& level) {
+    size_lv1 = level.chunks.size();
+    for(RadixLevel2& next : level.chunks)
+        calculate_on(next);
+}
+
+void RadixStats::calculate_on(RadixLevel2& level) {
+    free_lv2 += algo(count_if, level.chunk, [](uint32_t v) { return v == slot_empty; });
+    size_lv2 += lvl2_len - free_lv2;
+    perc_lv2 += 100. * size_lv2 / (size_lv1 * lvl2_len);
+
+    free_lv3 += algo(count_if, level.extra, [](uint32_t v) { return v == slot_empty; });
+    size_lv3 += lvl2_xtr - free_lv3;
+    perc_lv3 += 100. * size_lv3 / (size_lv1 * lvl2_xtr);
+}
+
+std::string RadixStats::to_string() const {
+    std::stringstream ss;
+    ss << "stats(" << std::endl;
+    ss << " free_lv0=" << free_lv0
+        << " take_lv0=" << take_lv0
+        << " chld_lv0=" << chld_lv0 << std::endl;
+    ss << " size_lv1=" << size_lv1 << std::endl;
+    ss << " size_lv2=" << size_lv2
+        << " free_lv2=" << free_lv2
+        << " perc_lv2=" << perc_lv2 << std::endl;
+    ss << " size_lv3=" << size_lv3
+        << " free_lv3=" << free_lv3
+        << " perc_lv3=" << perc_lv3 << std::endl;
+    ss << ")";
+    return ss.str();
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+std::vector<uint32_t> generate_rand_uint_input(uint32_t len, uint32_t max_item_val=max_number) {
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(0, max_item_val);
+
+    std::vector<uint32_t> input;
+    input.reserve(len);
+    for(uint32_t i=0; i<len; ++i)
+        input.push_back(dist(gen));
+    return input;
+}
+
+void sort_numbers_by_radix() {
+    RadixTree container;
+    auto input = generate_rand_uint_input(input_len);
+    for(auto n : input)
+        container.add_number(n);
+
+    RadixStats stats {};
+    stats.calculate_on(container);
+    LOG(stats.to_string());
+
+    auto sort_input = input;
+    algo_srt(sort_input);
+    size_t ref_idx = 0;
+    for(auto val : container.range()) {
+        if(val != sort_input[ref_idx])
+            LOG("at " << ref_idx << " : " << val << " != " << sort_input[ref_idx]);
+        MY_ASSERT(val == sort_input[ref_idx]);
+        ref_idx += 1;
+    }
+}
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 int main(void) {
-  sort_numbers_by_radix();
-  return 0;
+    sort_numbers_by_radix();
+    return 0;
 }
 
--- sorting_in_1mb_ram/radix_sorting.hpp
+++ sorting_in_1mb_ram/radix_sorting.hpp
@@ -3,10 +3,14 @@
 #include <cassert>
 #include <iterator>
 #include <list>
+#include <string>
 #include <vector>
 
 #pragma GCC diagnostic ignored "-Wunused-function"
 #define LOG(msg) std::cout << msg << std::endl;
+#define algo(name, v, pred) std::name(v.begin(), v.end(), pred)
+#define algo_acc(v, init, pred) std::accumulate(v.begin(), v.end(), init, pred)
+#define algo_srt(v) std::sort(v.begin(), v.end())
 
 #ifdef NDEBUG
 #define MY_ASSERT(x) do { (void)sizeof(x); } while (0)
@@ -17,103 +21,151 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 const static size_t input_len  = 1000000;
-const static size_t max_v_mask = 100000000;
-const static size_t lvl0_len   = 4096;
-const static size_t lvl0_shf   = 15;
-const static size_t lvl0_mask  = (1 << lvl0_shf) - 1;
-const static size_t lvl0_flag  = 1 << lvl0_shf;
-const static size_t lvl2_len   = 256;
-const static size_t lvl2_shf   = 7;
-const static size_t lvl2_mask  = (1 << lvl2_shf) - 1;
-const static size_t lvl2_flag  = 1 << lvl2_shf;
-const static size_t lvl3_len   = 4;
+const static size_t max_number = 100000000;
+const static size_t bits_len   = 27;
+const static size_t lvl0_shf   = 12;
+const static size_t lvl0_len   = 1 << lvl0_shf;
+const static size_t lvl0_mask  = lvl0_len - 1;
+const static size_t lvl0_flag  = 1 << (bits_len - lvl0_shf);
+const static size_t lvl2_shf   = 8;
+const static size_t lvl2_len   = 1 << lvl2_shf;
+const static size_t lvl2_mask  = lvl2_len - 1;
+const static size_t lvl2_flag  = 1 << (bits_len - lvl0_shf - lvl2_shf);
+const static size_t lvl2_xtr   = lvl2_len / 8;
 
 const static uint32_t slot_empty = 0;
 const static uint32_t slot_min_val = 1;
 
 template<class I>
 struct ItContainer {
-  I start_it;
-  I end_it;
-  I begin() const { return start_it; }
-  I end() const { return end_it; }
+    I start_it;
+    I end_it;
+    I begin() const { return start_it; }
+    I end() const { return end_it; }
 };
 
-struct RadixLvl3It {};
-struct RadixLvl2It {};
+struct RadixTree;
+struct RadixLevel1;
+struct RadixLevel2;
+
+struct RadixLvl2It {
+    using chunk_t = std::array<uint8_t, lvl2_len>;
+    using extra_t = std::array<uint16_t, lvl2_xtr>;
+    
+    const RadixLevel2* parent;
+    chunk_t::const_iterator chunk_it;
+    extra_t::const_iterator extra_it;
+
+    using value_type = uint32_t;
+    using difference_type = int32_t;
+    using pointer = const uint32_t*;
+    using reference = const uint32_t;
+    using iterator_category = std::input_iterator_tag;
+
+    reference operator*() const;
+    RadixLvl2It& operator++();
+    RadixLvl2It  operator++(int);
+
+    bool operator==(const RadixLvl2It& rhs) const;
+    bool operator!=(const RadixLvl2It& rhs) const;
+    void move_to_end();
+    void advance_chunk_skip_empty(bool=true);
+    uint32_t extract_from_chunk() const;
+    uint32_t extract_from_extra() const;
+};
 
 struct RadixLvl1It {
-  uint32_t id;
-  bool consumed;
-  RadixLvl2It next_it, next_end;
+    const RadixLevel1* parent;
+    uint32_t id;
+    RadixLvl2It next_it, next_end;
 
-  using value_type = uint32_t;
-  using difference_type = int32_t;
-  using pointer = const uint32_t*;
-  using reference = const uint32_t&;
-  using iterator_category = std::forward_iterator_tag;
+    using value_type = uint32_t;
+    using difference_type = int32_t;
+    using pointer = const uint32_t*;
+    using reference = const uint32_t;
+    using iterator_category = std::input_iterator_tag;
 
-  reference operator*();
-  RadixIt&  operator++();
-  RadixIt   operator++(int);
+    reference operator*() const;
+    RadixLvl1It& operator++();
+    RadixLvl1It  operator++(int);
 
-  bool operator==(const RadixIt& rhs) const;
-  bool operator!=(const RadixIt& rhs) const;
-  void move_to_end();
+    bool operator==(const RadixLvl1It& rhs) const;
+    bool operator!=(const RadixLvl1It& rhs) const;
+    void move_to_end();
 };
 
 struct RadixIt {
-  std::vector<uint32_t>::const_iterator last_resort_it, last_resort_end;
-  std::vector<uint16_t>::const_iterator chunk_it, chunk_begin, chunk_end;
-  RadixLvl1It next_it, next_end;
+    const RadixTree* parent;
+    std::vector<uint32_t>::const_iterator last_resort_it;
+    std::vector<uint16_t>::const_iterator chunk_it;
+    RadixLvl1It next_it, next_end;
 
-  using value_type = uint32_t;
-  using difference_type = int32_t;
-  using pointer = const uint32_t*;
-  using reference = const uint32_t&;
-  using iterator_category = std::forward_iterator_tag;
+    using value_type = uint32_t;
+    using difference_type = int32_t;
+    using pointer = const uint32_t*;
+    using reference = const uint32_t;
+    using iterator_category = std::input_iterator_tag;
 
-  reference operator*();
-  RadixLvl1It& operator++();
-  RadixLvl1It operator++(int);
+    reference operator*() const;
+    RadixIt& operator++();
+    RadixIt  operator++(int);
 
-  bool operator==(const RadixIt& rhs) const;
-  bool operator!=(const RadixIt& rhs) const;
-  void move_to_end();
-  uint32_t extract(uint32_t number);
+    bool operator==(const RadixIt& rhs) const;
+    bool operator!=(const RadixIt& rhs) const;
+    void move_to_end();
+    void advance_chunk_skip_empty(bool=true);
+    uint32_t extract_direct() const;
+    uint32_t extract_lower_lvl() const;
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-struct RadixLevel3 {
-  std::list<std::array<uint8_t, lvl3_len>> chunks;
-  uint32_t add_number(uint32_t number);
-  uint32_t add_number(uint32_t id, uint32_t number);
-  ItContainer<RadixLvl3It> range();
-};
-
 struct RadixLevel2 {
-  std::array<uint8_t, lvl2_len> chunk;
-  RadixLevel3 next_lvl;
-  uint32_t add_number(uint32_t number);
-  ItContainer<RadixLvl2It> range();
+    RadixLvl2It::chunk_t chunk;
+    RadixLvl2It::extra_t extra;
+    uint8_t extra_top;
+
+    RadixLevel2();
+    uint32_t add_number(uint32_t number);
+    uint32_t add_to_extra(uint32_t number);
+    ItContainer<RadixLvl2It> range() const;
+    uint32_t to_chunk_payload(uint32_t number) const;
+    uint32_t to_extra_payload(uint32_t number) const;
 };
 
 struct RadixLevel1 {
-  std::list<RadixLevel2> chunks;
-  RadixLevel1();
-  uint32_t add_number(uint32_t number);
-  uint32_t add_number(uint32_t id, uint32_t number);
-  ItContainer<RadixLvl1It> range(uint32_t id);
+    std::vector<RadixLevel2> chunks;
+
+    RadixLevel1();
+    uint32_t add_number(uint32_t number);
+    uint32_t add_number(uint32_t id, uint32_t number);
+    ItContainer<RadixLvl1It> range(uint32_t id) const;
+    RadixLvl1It empty_it() const;
 };
 
 struct RadixTree {
-  std::vector<uint32_t> last_resort;
-  std::vector<uint16_t> chunk;
-  RadixLevel2 next_lvl;
+    std::vector<uint32_t> last_resort;
+    std::vector<uint16_t> chunk;
+    RadixLevel1 next_lvl;
 
-  RadixTree();
-  uint32_t add_number(uint32_t number);
-  ItContainer<RadixLvl1It> range();
+    RadixTree();
+    uint32_t add_number(uint32_t number);
+    uint32_t add_to_last_resort(uint32_t number);
+    ItContainer<RadixIt> range() const;
+    uint32_t to_chunk_payload(uint32_t number) const;
+    uint32_t from_chunk_payload(uint32_t slot) const;
 };
 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+struct RadixStats {
+    uint32_t free_lv0, take_lv0, chld_lv0;
+    uint32_t size_lv1;
+    uint32_t size_lv2, free_lv2, perc_lv2;
+    uint32_t size_lv3, free_lv3, perc_lv3;
+
+    void calculate_on(RadixTree&);
+    void calculate_on(RadixLevel1&);
+    void calculate_on(RadixLevel2&);
+    std::string to_string() const;
+};
