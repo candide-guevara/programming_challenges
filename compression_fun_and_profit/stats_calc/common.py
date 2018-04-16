prob distribution calculation -bug fixes

compression_fun_and_profit/stats_calc/common.py
@@ -1,6 +1,12 @@
 import numpy as np, gzip
 import logging, sys, os, re, argparse, gzip, uuid, datetime
 
+MAX_PRICE_RATIO = 10**6
+MAX_ABS_PRICE   = 10**8
+MIN_SCALE       = 40_000
+MIN_DATAPOINTS  = 50
+END_MARK        = None
+
 class DFormat:
   RAW,NORMAL,DELTA = 0,1,2
 
@@ -37,7 +43,7 @@ def intlen_to_nptype(config):
 
 def dict_to_np_pairs(dico, key_t, val_t):
   dtype=[('key', key_t), ('val', val_t)]
-  return np.fromiter(dico.items(), dtype=dtype)
+  return np.fromiter(dico.items(), dtype=dtype, count=len(dico))
 
 def parse_args (help_msg):
   parser = argparse.ArgumentParser(help_msg)
@@ -58,15 +64,9 @@ def parse_args (help_msg):
   parser.add_argument ('--int-len',
                         help='The len of each normalized price in bits',
                         default=32)
-  parser.add_argument ('--min-max-equal',
-                        help='The tolerance for considering the series is constant (thus discarding it)',
-                        default=1e-3)
-  parser.add_argument ('--min-max-ratio',
-                        help='The tolerance for series max/min ration before discarding',
-                        default=1000000)
   parser.add_argument ('--alphabet-len',
                         help='The number of symbols inside the alphabet for prob distribution',
-                        default=29)
+                        default=64)
   args = parser.parse_args()
   return args
 

compression_fun_and_profit/stats_calc/series_stats_calc.py
@@ -2,8 +2,6 @@ import numpy as np, math
 from common import *
 logger = logging.getLogger(__name__)
 
-END_MARK = None
-
 class Histogram:
 
   def __init__(self):
@@ -25,7 +23,7 @@ class Histogram:
     return sum( (w/self.count) * math.log2(self.count/w) for w in self.buckets.values() )
 
   def calc_stats(self, config):
-    logger.info('calculating stats for %d buckets, ratio %f', len(self.buckets), len(self.buckets)/self.count)
+    logger.info('calculating stats for %d buckets, ratio %f/%f', len(self.buckets), len(self.buckets), self.count)
     count, self.avg, self.std = 0,0.0,0.0
     dtype = intlen_to_nptype(config)
     pairs = dict_to_np_pairs(self.buckets, dtype, np.uint64)
@@ -126,6 +124,11 @@ class SeriesStats:
 
 ### END SeriesStats
 
+def filter_not_relevant_series(data):
+  if len(data) < MIN_DATAPOINTS:
+    return True
+  return False
+
 # the alphabet will be composed of :
 # [ ... -alphabet_len**2, -alphabet_len, ... 0, 1, ... alphabet_len, alphabet_len**2 ... END_MARK ]
 def calc_stats_from_delta_series(config, series):
@@ -133,9 +136,11 @@ def calc_stats_from_delta_series(config, series):
   stats = SeriesStats(config)
 
   for progress,data in enumerate( d.base for d in series.data ):
-    if progress % 100 == 0:
-      logger.info('progress: loading stats for %r', series.meta[progress])
-    counts = np.unique(data, return_counts=True)
+    if filter_not_relevant_series(data): continue
+    if progress % 200 == 0:
+      logger.info('progress[%d]: %r', int(100*progress/series.count), series.meta[progress])
+    # we do not count the first element because it is not a delta
+    counts = np.unique(data[1:], return_counts=True)
     stats.propose_min_max(counts[0][0], counts[0][-1])
     for val,weight in zip(*counts): 
       stats.add_to_buckets(val, weight)

compression_fun_and_profit/stats_calc/series_transform.py
@@ -6,16 +6,28 @@ logger = logging.getLogger(__name__)
 
 def is_series_abnormal(config, meta):
   assert meta.min >= 0
-  if meta.max/(1+meta.min) > config.min_max_ratio:
+  if meta.max/(1+meta.min) > MAX_PRICE_RATIO:
     logger.info('rejecting %r because max/min is too wide', meta)
     return True
-  if math.isclose(meta.min, meta.max, rel_tol=config.min_max_equal):
-    logger.info('rejecting %r because min == max', meta)
-    return True
-  if meta.max > 10**9:
+  if meta.max > MAX_ABS_PRICE:
     logger.info('rejecting %r because max price is too high', meta)
     return True
   return False
+
+# if (vmin-vmax) is too small, it is overkill to scale to the whole available range
+def choose_suitable_range(vmin, vmax):
+  vrange = (vmin, vmax)
+  if (vmax - vmin) < MIN_SCALE:
+    vrange = (vmin, vmin+MIN_SCALE)
+  assert vrange[0] < vrange[1]
+  return vrange
+
+def highlight_wtf_deltas(config, meta, deltas):
+  #assert any(deltas.compressed() < 0)
+  index = np.absolute(deltas) > 2 ** (config.int_len - 4)
+  wtf = deltas[index].compressed()
+  if len(wtf):
+    logger.warn("Got huge deltas %r -> %r", meta, wtf)
 
 def normalize_series(config, series):
   assert series.dformat == DFormat.RAW
@@ -26,8 +38,7 @@ def normalize_series(config, series):
   for meta,data in zip(series.meta, series.data):
     assert not data.mask[0], 'serie should start with non Na : %r\n%r' % (meta,data)
     new_meta = copy.copy(meta)
-    new_meta.min = data.min()
-    new_meta.max = data.max()
+    new_meta.min, new_meta.max = choose_suitable_range(data.min(), data.max())
     if is_series_abnormal(config, new_meta):
       continue
     new_data = lerp_max * ((data - new_meta.min) / (new_meta.max - new_meta.min))
@@ -41,9 +52,13 @@ def normal_to_delta_series(config, series):
   new_series = Series(DFormat.DELTA)
   for meta,data in zip(series.meta, series.data):
     new_meta = copy.copy(meta)
-    new_data = np.ediff1d(data.filled(0), to_begin=data[:1])
+    new_data = np.ediff1d(data.compressed(), to_begin=data[:1])
+    nan_index = np.flatnonzero(data.mask)
+    nan_index -= np.arange(len(nan_index))
+    new_data = np.insert(new_data, nan_index, 0)
     new_data = np.ma.MaskedArray(new_data, data.mask)
     new_series.add(new_meta, new_data)
+    #highlight_wtf_deltas(config, meta, new_data)
   logger.info("to delta %d series", series.count)
   return new_series
 

compression_fun_and_profit/stats_calc/test.py
@@ -68,6 +68,13 @@ class TestSeriesIO (ut.TestCase):
     expected_lines = sum( len(d) + 1 for d in series.data )
     self.assertGreaterEqual(len(lines), expected_lines)
 
+    series = series_transform.normalize_series(self.config, series)
+    dump_path = series_io.dump_as_plain_txt(
+      TestSeriesIO.config, tmp_stage_name(TestSeriesIO.config), series)
+    series = series_transform.normal_to_delta_series(self.config, series)
+    dump_path = series_io.dump_as_plain_txt(
+      TestSeriesIO.config, tmp_stage_name(TestSeriesIO.config), series)
+
   #@ut.skip('')
   def test_dump_prob_distribution(self):
     series = build_gaussian_series(self.config, 0, 128, 10, 2000)
@@ -81,7 +88,7 @@ class TestSeriesIO (ut.TestCase):
 
 ### END TestSeriesIO
 
-@ut.skip('')
+#@ut.skip('')
 class TestSeriesTransform (ut.TestCase):
 
   @classmethod
@@ -102,8 +109,11 @@ class TestSeriesTransform (ut.TestCase):
       self.assertEqual(meta1.count, meta2.count)
       self.assertEqual(meta1.start, meta2.start)
     for meta2,data2 in zip(norm_series.meta, norm_series.data):
+      vmin, vmax = data2.min(), data2.max()
       self.assertTrue(np.any(data2 == 0))
-      self.assertTrue(np.any(data2 == lerp_max), '%r = %d/%d' % (meta2, data2.min(), data2.max()))
+      self.assertTrue(meta2.max - meta2.min >= MIN_SCALE)
+      self.assertTrue(vmax - vmin >= 0, "%r < %r" % (vmin,vmax))
+      self.assertTrue(np.all(data2 <= lerp_max), '%r = [%d, %d]' % (meta2, vmin, vmax))
 
   #@ut.skip('')
   def test_cycle_transformations(self):
@@ -111,6 +121,7 @@ class TestSeriesTransform (ut.TestCase):
     norm_series = series_transform.normalize_series(self.config, series)
     delta_series = series_transform.normal_to_delta_series(self.config, norm_series)
     raw_series = series_transform.delta_to_raw_series(self.config, delta_series)
+    atol = MIN_SCALE / 2 ** (self.config.int_len-1)
 
     #logger.debug('start : %r\nend : %r', series.data[2], raw_series.data[2])
     for meta1,meta2 in zip(series.meta, raw_series.meta):
@@ -118,11 +129,14 @@ class TestSeriesTransform (ut.TestCase):
       self.assertEqual(meta1.count, meta2.count)
       self.assertEqual(meta1.start, meta2.start)
     for data1,data2 in zip(series.data, raw_series.data):
-      self.assertTrue(np.allclose(data1, data2, rtol=1e-3), '\n%r\n%r' % (data1, data2))
+      outliers = ((data1 - data2) / data2).compressed()
+      outliers = outliers[np.logical_or(outliers<-1e-3, outliers>1e-3)]
+      self.assertTrue(np.array_equal(data1.mask, data2.mask))
+      self.assertTrue(np.allclose(data1, data2, atol=atol, rtol=1e-3), '\n%r' % outliers)
 
 ### END TestSeriesTransform
 
-@ut.skip('')
+#@ut.skip('')
 class TestStatCalculator (ut.TestCase):
 
   @classmethod