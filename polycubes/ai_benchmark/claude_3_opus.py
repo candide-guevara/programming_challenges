cfbdf9a97af801383d862621db16a43b0418d8d1
candide guevara <candide-guevara@users.noreply.github.com>
2025-02-01 13:37:41 +0100

    try polycube bench on claude opus 3, attempt#1

---
+++ polycubes/ai_benchmark/claude_3_opus.py
@@ -0,0 +1,65 @@
+import numpy as np
+from scipy.spatial.distance import cdist
+import logging
+from utils.common import *
+
+# Attempt1:
+# Do you know what the counting 3D polycubes problem is? If so, do you know the solutions for the first 2 to 6 cube counts?
+#
+# Can you write a python program to count the number of possible 3D polycubes for any number of individual cubes? Use numpy to make the counting as fast as possible.
+#
+# Result Attempt1: results are wrong
+
+def is_connected(cubes):
+    """Check if all cubes are connected."""
+    dist = cdist(cubes, cubes, metric='chebyshev') <= 1
+    visited = set()
+    
+    def dfs(cube):
+        visited.add(tuple(cube))
+        for neighbor in cubes[np.any(dist[tuple(cube)], axis=0)]:
+            if tuple(neighbor) not in visited:
+                dfs(neighbor)
+    
+    dfs(cubes[0])
+    return len(visited) == len(cubes)
+
+def normalize_polycube(cubes):
+    """Translate polycube to origin and sort cubes."""
+    cubes = cubes - np.min(cubes, axis=0)
+    return tuple(map(tuple, sorted(cubes)))
+
+def unique_polycubes(n):
+    """Generate all unique polycubes of size n."""
+    if n == 1:
+        return {((0,0,0),)}
+
+    polycubes = set()
+    for polycube in unique_polycubes(n-1):
+        polycube = np.array(polycube) 
+        for cube in polycube:
+            for direction in np.eye(3, dtype=int):
+                new_cube = tuple(cube + direction)
+                if new_cube not in polycube:
+                    new_polycube = normalize_polycube(np.concatenate((polycube, [new_cube])))
+                    polycubes.add(new_polycube)
+    
+    return polycubes
+
+def count_polycubes(n):
+    """Count the number of unique 3D polycubes of size n."""
+    return len(unique_polycubes(n))
+
+def main():
+	for size in range(3, MAX_SIZE):
+		with print_time(""):
+			polycubes = count_polycubes(size)
+			logging.info("size=%d, count=%d", size, polycubes)
+			is_ok(size, polycubes)
+	logging.warning("DONE")
+
+if __name__ == '__main__':
+	logging.basicConfig(level=logging.INFO,
+											format='[%(levelname)s] %(name)s::%(funcName)s  %(message)s')
+	main()
+
